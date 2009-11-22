1c091f186c8c48cdcede59a171802635bd98500f
travisutk <travisutk@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2009-11-22 03:20:03 +0000

    Switched from explicit port definitions in asm to preprocessor definitions.  gfports.h, being included in assembly, MUST NOT contain any C function definitions.
    This fixes MSP430 flash programming from the GoodFET30.
    
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@230 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

--- trunk/firmware/include/platform.h
+++ trunk/firmware/include/platform.h
@@ -10,26 +10,7 @@
 #include <io.h>
 #include <iomacros.h>
 
-
-//Use P3 instead of P5 for target I/O on chips without P5.
-#ifndef __MSP430_HAS_PORT5__
-#ifndef __MSP430_HAS_PORT5_R__
-#warning "No P5, using P3 instead.  Will break 2618 and 1612 support."
-#define P5OUT P3OUT
-#define P5DIR P3DIR
-#define P5REN P3REN
-#define P5IN P3IN
-
-#endif
-#endif
-
-//Use false P5REN for 1612.
-#ifdef __MSP430_HAS_PORT5__
-#ifndef __MSP430_HAS_PORT5_R__
-#warning "1xx, using fake P5REN for external pulling resistors."
-#define P5REN P5OUT
-#endif
-#endif
+#include "gfports.h"
 
 unsigned char serial_rx();
 void serial_tx(unsigned char);

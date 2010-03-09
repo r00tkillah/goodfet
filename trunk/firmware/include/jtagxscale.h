2a1ca5137e35423511d0a3d60e0f3e157f517520
dwhuseby <dwhuseby@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2010-03-09 22:41:24 +0000

    stubbin in the xscale specific header.
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@400 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

---
+++ trunk/firmware/include/jtagxscale.h
@@ -0,0 +1,11 @@
+/*! \file jtagxscale.h
+  \author Dave Huseby <huseby at linuxprogrammer.org>
+  \brief Intel XScale JTAG command verbs
+*/
+
+
+/*
+ * TODO: specify the standard Intel XScale IR, TCB control and device specific
+ * values.
+ */
+

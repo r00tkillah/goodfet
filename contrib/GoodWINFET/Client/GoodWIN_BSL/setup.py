e634e1757b74510e60b00e068abb549d4e10ec77
theqlabs <theqlabs@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2010-05-15 07:28:54 +0000

    Updated GoodWINFET/Client/GoodWIN_BSL - includes all necessary files and depends for Windows binary
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@506 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

---
+++ contrib/GoodWINFET/Client/GoodWIN_BSL/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# GoodFET Windows Installer
+# 
+# Written poorly by Q - <q@theqlabs.com>
+#
+# This code is being rewritten and refactored.  You've been warned!
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['goodfet.bsl'])

01782fd9e37cfbe4859b142be7f0bd638ec5a268
travisutk <travisutk@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2010-05-19 23:16:53 +0000

    NordicRF Registers
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@531 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

--- trunk/firmware/include/nrf.h
+++ trunk/firmware/include/nrf.h
@@ -16,4 +16,36 @@
 
 
 //NRF24L01+ Registers
-//TODO add these
+//These aren't yet used, but are included for later
+//translation to XML.
+#define NRF_CONFIG      0x00
+#define NRF_EN_AA       0x01
+#define NRF_EN_RXADDR   0x02
+#define NRF_SETUP_AW    0x03
+#define NRF_SETUP_RETR  0x04
+#define NRF_RF_CH       0x05
+#define NRF_RF_SETUP    0x06
+#define NRF_STATUS      0x07
+#define NRF_OBSERVE_TX  0x08
+#define NRF_RPD         0x09
+#define NRF_RX_ADDR_P0  0x0A
+#define NRF_RX_ADDR_P1  0x0B
+#define NRF_RX_ADDR_P2  0x0C
+#define NRF_RX_ADDR_P3  0x0D
+#define NRF_RX_ADDR_P4  0x0E
+#define NRF_RX_ADDR_P5  0x0F
+#define NRF_TX_ADDR     0x10
+#define NRF_RX_PW_P0    0x11
+#define NRF_RX_PW_P1    0x12
+#define NRF_RX_PW_P2    0x13
+#define NRF_RX_PW_P3    0x14
+#define NRF_RX_PW_P4    0x15
+#define NRF_RX_PW_P5    0x16
+#define NRF_FIFO_STATUS 0x17
+#define NRF_DYNPD       0x1C
+//Also 32-byte buffers for ACK_PLD, TX_PLD, and RX_PLD.
+//Separate SPI commands.
+
+
+
+

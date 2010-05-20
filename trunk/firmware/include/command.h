9fe99c605c24a46d0a2c88855ad80482d6bd7e79
travisutk <travisutk@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2010-05-20 14:35:32 +0000

    Moved monitor commands.
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@543 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

--- trunk/firmware/include/command.h
+++ trunk/firmware/include/command.h
@@ -48,23 +48,6 @@ extern unsigned char silent;
 
 #define DEBUGSTR 0xFF
 
-// Monitor Commands
-#define MONITOR_CHANGE_BAUD 0x80
-#define MONITOR_RAM_PATTERN 0x90
-#define MONITOR_RAM_DEPTH 0x91
-
-#define MONITOR_DIR 0xA0
-#define MONITOR_OUT 0xA1
-#define MONITOR_IN  0xA2
-
-#define MONITOR_SILENT 0xB0
-#define MONITOR_CONNECTED 0xB1
-
-#define MONITOR_READBUF 0xC0
-#define MONITOR_WRITEBUF 0xC1
-#define MONITOR_SIZEBUF 0xC2
-
-
 
 
 //SPI commands

4597750f04426adbce4b4b98c5f835ee80237616
travisutk <travisutk@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2010-07-08 17:50:31 +0000

    RNG test.
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@670 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

--- trunk/client/GoodFETEM260.py
+++ trunk/client/GoodFETEM260.py
@@ -23,40 +23,68 @@ from GoodFETSPI import GoodFETSPI;
 class GoodFETEM260(GoodFETSPI):
     EM260APP=0x01;
     seq=0;
-    def EM260trans(self,data):
+    def EM260trans(self,data,retry=1):
         """Exchange data by EM260 SPI. (Slightly nonstandard.)"""
-        self.data=data;
+        
+        if retry==0:
+            #Retries exceeded.  Send a trivial command to clear error.
+            data=[0x0A,0xA7];
         self.writecmd(0x01,0x82,len(data),data);
+        
         
         try:
             reply=ord(self.data[0]);
             if(reply==0x00):
-                print "Warning: EM260 rebooted.";
-                return self.EM260trans(data);
+                #print "Warning: EM260 rebooted.";
+                return self.EM260trans(data,retry-1);
             if(reply==0x02):
-                print "Error: Aborted Transaction.";
-                #return self.EM260trans(data);
+                #print "Error: Aborted Transaction.";
+                return self.EM260trans(data,retry-1);
             if(reply==0x03):
                 print "Error: Missing Frame Terminator.";
-                #return self.data;
+                return self.data;
+            if(reply==0x04):
+                print "Error: Reserved Error.  (Access denied?)";
+                return self.data;
         except:
             print "Error in EM260trans.";
         return self.data;
     
-    
+    def EZSPtrans(self,frame):
+        """Send an EZSP frame."""
+        data=self.EM260trans([0xFE,len(frame)+2,
+                              self.seq,0x00,
+                              ]+frame+[
+                              0xA7]);
+        #s="";
+        #for foo in data:
+        #    s=s+"%02x " % ord(foo);
+        #print s;
+        if ord(data[0])!=0xFE:
+            print "EZSP error: 0x%02x" % ord(data[0]);
+        if frame[0]!=ord(data[4]):
+            print "EZSP warning: Command 0x%02x returned type 0x%02x." % (
+                frame[0],ord(data[4]));
+        self.seq=self.seq+1;
+        return data;
+        #return ord(data[0]);
+        
     def peek8(self,adr):
         """Read a byte from the given address."""
-        data=self.EM260trans([0xfe,0x01,self.seq,0x00,
-                            0x49,
-                            0xA7]);
-        s="";
-        for foo in data:
-            s=s+"%02x " % ord(foo);
-        print s;
+        
+        data=self.EZSPtrans([0x47,adr&0xFF]);
         
         return ord(data[0]);
+    
+    def rand16(self):
+        """Read a byte from the given address."""
+        
+        data=self.EZSPtrans([0x49]);
+        return ord(data[6])+(ord(data[7])<<8);
+    
     def info(self):
         """Read the info bytes."""
+        print "Ember EM26 Z-Stack SPI Module.";
         version=self.EM260spiversion();
         status=self.EM260spistatus();
         print "Version: %i" % (version); 

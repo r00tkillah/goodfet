fccb2c30ed9e9fdfd331e05057b8f3fc4fbaed25
travisutk <travisutk@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2010-09-05 21:01:24 +0000

    MSP430X2 corrections.
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@721 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

--- trunk/firmware/apps/jtag/jtag430x2.c
+++ trunk/firmware/apps/jtag/jtag430x2.c
@@ -93,19 +93,20 @@ unsigned int jtag430x2_readmem(unsigned long adr){
   //unsigned int tries=5;
   
   while(1){
-    do{
-      jtag_ir_shift8(IR_CNTRL_SIG_CAPTURE);
-    }while(!(jtag_dr_shift16(0) & 0x0301));
+    //do{
+    jtag_ir_shift8(IR_CNTRL_SIG_CAPTURE);
+    //}while(!(jtag_dr_shift16(0) & 0x0301));
     
     if(jtag_dr_shift16(0) & 0x0301){
       // Read Memory
       CLRTCLK;
       jtag_ir_shift8(IR_CNTRL_SIG_16BIT);
-      if(adr>=0x100){
-	jtag_dr_shift16(0x0501);//word read
-      }else{
-	jtag_dr_shift16(0x0511);//byte read
-      }
+      
+      //if(adr>=0x100){
+      jtag_dr_shift16(0x0501);//word read
+      //}else{
+      //jtag_dr_shift16(0x0511);//byte read
+      //}
       
       jtag_ir_shift8(IR_ADDR_16BIT);
       jtag_dr_shift20(adr); //20

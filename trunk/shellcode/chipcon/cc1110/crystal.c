63668f1def906ab1aa11e3c6cf5ffaae360f8f3a
travisutk <travisutk@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2011-01-22 23:14:19 +0000

    Nothing major, just CC1110 crystal routines.
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@855 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

--- trunk/shellcode/chipcon/cc1110/crystal.c
+++ trunk/shellcode/chipcon/cc1110/crystal.c
@@ -8,10 +8,22 @@ void main(){
   // Wait until xtal oscillator is stable
   while( !(SLEEP & SLEEP_XOSC_S) ); 
 
+  
+  
   // Select xtal osc, 26 MHz
+  // This doesn't work for the USB dongles.
   CLKCON = 
     (CLKCON & ~(CLKCON_CLKSPD | CLKCON_OSC))
-    | CLKSPD_DIV_1; 
+    | CLKSPD_DIV_1;
+  
+  
+  /*
+  //Needed for CC1111?
+  CLKCON = 
+    (CLKCON & ~(CLKCON_CLKSPD | CLKCON_OSC))
+    | CLKSPD_DIV_2;
+  */
+  
   // Wait for change to take effect
   while (CLKCON & CLKCON_OSC); 
   // Turn off the RC osc.

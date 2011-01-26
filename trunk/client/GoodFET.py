ea63bdd6fe45af4bd3ecc4af155c04d9b319bd4a
travisutk <travisutk@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2011-01-26 16:52:18 +0000

    Sets the timeout to 5 after connection, might fix 1612 errors.
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@861 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

--- trunk/client/GoodFET.py
+++ trunk/client/GoodFET.py
@@ -150,6 +150,7 @@ class GoodFET:
                     break;
         if self.verbose: print "Connected after %02i attempts." % attempts;
         self.mon_connected();
+        self.serialport.setTimeout(5);
     def telosSetSCL(self, level):
         self.serialport.setRTS(not level)
     def telosSetSDA(self, level):

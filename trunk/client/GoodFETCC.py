87fe7790213bf7f36d4beb0571bd56713845e2a1
travisutk <travisutk@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2011-04-24 16:39:18 +0000

    SmartRF 7 is now found in Windows Vista 64-bit. (64-bit Windows, 32-bit Python.)
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@1009 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

--- trunk/client/GoodFETCC.py
+++ trunk/client/GoodFETCC.py
@@ -24,7 +24,10 @@ class GoodFETCC(GoodFET):
         if self.smartrfpath==None:
             self.smartrfpath=os.environ.get("SMARTRF");
         if self.smartrfpath==None and os.name=='nt':
-            self.smartrfpath="c:/Program Files/Texas Instruments/SmartRF Tools/SmartRF Studio 7";
+            self.smartrfpath="c:/Program Files (x86)/Texas Instruments/SmartRF Tools/SmartRF Studio 7";
+            if not os.path.exists(self.smartrfpath):
+                self.smartrfpath="c:/Program Files/Texas Instruments/SmartRF Tools/SmartRF Studio 7";
+            
         if self.smartrfpath==None:
             self.smartrfpath="/opt/smartrf7";
         

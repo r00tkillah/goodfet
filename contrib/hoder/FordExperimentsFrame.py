42ef5f1bd99e37c5ba0fb44f986e27839816a80c
chrishoder <chrishoder@12e2690d-a6be-4b82-a7b7-67c4a43b65c8>
2013-02-24 02:37:05 +0000

    debugging changes
    git-svn-id: https://svn.code.sf.net/p/goodfet/code@1530 12e2690d-a6be-4b82-a7b7-67c4a43b65c8

--- contrib/hoder/FordExperimentsFrame.py
+++ contrib/hoder/FordExperimentsFrame.py
@@ -6,16 +6,17 @@ tk = Tkinter
 class FordExperimentsFrame:
 
     def __init__(self, frame, communicationLink):
+        i = 0
         self.comm = communicationLink
         entryLabel = tk.Label(frame, text="Ford Focus 2004 -- High Speed CAN demonstrations", font = "Helvetica 16 bold italic")
-        entryLabel.grid(row=i,column=0,columnspan=3,sticky=tk.W)
+        entryLabel.grid(row=i,column=0,columnspan=5,sticky=tk.W)
         
         i += 1
         entryLabel = tk.Label(frame, text="Set Speedometer:")
         entryLabel.grid(row=i,column=0,sticky=tk.W)
         self.speedVar = tk.StringVar()
         self.speedVar.set("")
-        entryWidget = tk.Entry(frame, textvariable=self.speedVar)
+        entryWidget = tk.Entry(frame, textvariable=self.speedVar,width=5)
         entryWidget.grid(row=i,column=1,sticky=tk.W)
         b = tk.Button(frame, command=self.setSpeedometer, text="Run")
         b.grid(row=i,column=2,sticky=tk.W)
@@ -26,7 +27,7 @@ class FordExperimentsFrame:
         entryLabel.grid(row=i,column=0,sticky=tk.W)
         self.rpmVar = tk.StringVar()
         self.rpmVar.set("")
-        entryWidget = tk.Entry(frame, textvariable= self.rpmVar)
+        entryWidget = tk.Entry(frame, textvariable= self.rpmVar, width=5)
         entryWidget.grid(row=i,column=1,sticky=tk.W)
         b = tk.Button(frame, command=self.setRPM, text = "Run")
         b.grid(row=i,column=2,sticky=tk.W)
@@ -37,7 +38,7 @@ class FordExperimentsFrame:
         entryLabel.grid(row=i,column=0, sticky=tk.W)
         self.engineTempVar = tk.StringVar()
         self.engineTempVar.set("")
-        entryWidget = tk.Entry(frame, textvariable=self.engineTempVar)
+        entryWidget = tk.Entry(frame, textvariable=self.engineTempVar, width=5)
         entryWidget.grid(row=i,column=1,sticky=tk.W)
         b = tk.Button(frame, command = self.setEngineTemp, text="Run")
         b.grid(row=i,column=2,sticky=tk.W)
@@ -60,7 +61,7 @@ class FordExperimentsFrame:
                 'Input is not an integer')
         #CALL METHOD    
         
-    def setRPM(self):
+    def setEngineTemp(self):
         try:
             engineTemp = int(self.engineTempVar.get())
         except:
